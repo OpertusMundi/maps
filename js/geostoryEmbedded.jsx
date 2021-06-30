a41424fa2c2a6bb2e3bddfc3726229c371cadd91
Daniel Kastl <daniel@georepublic.de>
2021-06-30 17:21:41 +0900

    Adds v0.5.0
    Signed-off-by: Daniel Kastl <daniel@georepublic.de>

---
+++ js/geostoryEmbedded.jsx
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2021, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import {
+    setConfigProp,
+    setLocalConfigurationFile
+} from '@mapstore/utils/ConfigUtils';
+/**
+ * Add custom (overriding) translations with:
+ *
+ * ConfigUtils.setConfigProp('translationsPath', ['./MapStore2/web/client/translations', './translations']);
+ */
+setConfigProp('translationsPath', './js/translations');
+setConfigProp('themePrefix', 'topiomaps');
+
+/**
+ * Use a custom plugins configuration file with:
+ *
+ * ConfigUtils.setLocalConfigurationFile('localConfig.json');
+ */
+setLocalConfigurationFile('localConfig.json');
+
+import('@mapstore/product/geostoryEmbedded');
