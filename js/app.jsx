93bced1fcfbd82c51d7b89bbfb323772f898ee0a
nleond <nikleonidakis@gmail.com>
2022-06-23 16:49:05 +0300

    First topio maps implementation

---
+++ js/app.jsx
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2016, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * Add custom (overriding) css file:ationsPath', ['./MapStore2/web/client/translations', './translations']);
+ */
+import '../assets/css/custom.css';
+
+import { checkForMissingPlugins } from '@mapstore/utils/DebugUtils';
+import main from '@mapstore/product/main';
+const ConfigUtils = require('@mapstore/utils/ConfigUtils').default;
+/**
+ * Add custom (overriding) translations with:
+ *
+ * ConfigUtils.setConfigProp('translationsPath', ['./MapStore2/web/client/translations', './translations']);
+ */
+ConfigUtils.setConfigProp('translationsPath', './js/translations');
+ConfigUtils.setConfigProp('themePrefix', 'maps');
+
+/**
+ * Use a custom plugins configuration file with:
+ *
+ * ConfigUtils.setLocalConfigurationFile('localConfig.json');
+ */
+ConfigUtils.setLocalConfigurationFile('./configs/localConfig.json');
+
+/**
+ * Use a custom application configuration file with:
+ *
+ * const appConfig = require('./appConfig');
+ *
+ * Or override the application configuration file with (e.g. only one page with a mapviewer):
+ *
+ * const appConfig = assign({}, require('@mapstore/product/appConfig'), {
+ *     pages: [{
+ *         name: "mapviewer",
+ *         path: "/",
+ *         component: require('@mapstore/product/pages/MapViewer')
+ *     }]
+ * });
+ */
+const appConfig = require('@mapstore/product/appConfig').default;
+
+/**
+ * Define a custom list of plugins with:
+ *
+ * const plugins = require('./plugins');
+ */
+const plugins = require('@mapstore/product/plugins').default;
+
+checkForMissingPlugins(plugins.plugins);
+
+main(appConfig, plugins);
